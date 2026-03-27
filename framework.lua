61c083c234ec630332be14036c24ac31cdd96b68
StarsationSetanya <tawan009911@gmail.com>
2026-03-27 16:31:20 +0700

    framework.lua

--- framework.lua
+++ framework.lua
@@ -205,7 +205,7 @@ do
 		end
 	end
 
-	local IconList = loadstring(game:HttpGet('https://raw.githubusercontent.com/Dummyrme/Library/refs/heads/main/Icon.lua'))()
+	local IconList = loadstring(game:HttpGet('https://raw.githubusercontent.com/StarsationSetanya/main/refs/heads/main/icons.lua'))()
 	function gl(i)
 		local iconData = IconList.Icons[i]
 		if iconData then
